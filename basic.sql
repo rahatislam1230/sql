074a1ed75c68599cccac3d53b31e61c6c1384a91
Rahad Islam <rahatislam2100@gmail.com>
2025-07-15 18:23:22 +0600

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -1,1 +1,6 @@
 SELECT employee_id,name,salary,salary*2+10 as rahat FROM employee where job='Waiter';
+SELECT name,salary,salary+ salary- 1000 * 50 / 100 
+FROM employee 
+where job ='Cook';
+select * from employee where manager_id is null;
+//null is not avabibale kisu nai not 0
